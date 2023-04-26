936236da17d659b7728f82b0770d361bf8900ee7
Luis Jonathan Carlosama <jonas_cj10k@outlook.com>
2023-04-26 18:47:07 -0500

    sub

---
+++ public/js/theme.js
@@ -0,0 +1,112 @@
+/////////////////////////////////////////////////////////////////////
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+/////////////////////////////////////////////////////////////////////
+
+$('.page-scroll').bind('click', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top -64
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
+});
+
+
+
+////////////////////////////////////////////////////////////////////////
+// On-Scroll Animated Header: https://github.com/codrops/AnimatedHeader
+////////////////////////////////////////////////////////////////////////
+
+var cbpAnimatedHeader = (function() {
+
+    var docElem = document.documentElement,
+        header = document.querySelector( '.navbar-fixed-top' ),
+        didScroll = false,
+        changeHeaderOn = 10;
+
+    function init() {
+        window.addEventListener( 'scroll', function( event ) {
+            if( !didScroll ) {
+                didScroll = true;
+                setTimeout( scrollPage, 250 );
+            }
+        }, false );
+    }
+
+    function scrollPage() {
+        var sy = scrollY();
+        if ( sy >= changeHeaderOn ) {
+            classie.add( header, 'navbar-shrink' );
+        }
+        else {
+            classie.remove( header, 'navbar-shrink' );
+        }
+        didScroll = false;
+    }
+
+    function scrollY() {
+        return window.pageYOffset || docElem.scrollTop;
+    }
+
+    init();
+
+})();
+
+
+
+//////////////////////////////////////////////
+// Highlight the top nav as scrolling occurs
+//////////////////////////////////////////////
+
+$('body').scrollspy({
+    target: '.navbar',
+    offset: 65
+})
+
+
+
+///////////////////////////////////////////
+// Display loading image while page loads
+///////////////////////////////////////////
+
+// Wait for window load
+$(window).load(function() {
+    // Animate loader off screen
+    $(".page-loader").fadeOut("slow");
+});
+
+
+
+////////////////////////////////////////////////////
+// OWL Carousel: http://owlgraphic.com/owlcarousel
+////////////////////////////////////////////////////
+
+// Intro text carousel
+$("#owl-intro-text").owlCarousel({
+    singleItem : true,
+    autoPlay : 6000,
+    stopOnHover : true,
+    navigation : false,
+    navigationText : false,
+    pagination : true
+})
+
+
+// Partner carousel
+$("#owl-partners").owlCarousel({
+    items : 4,
+    itemsDesktop : [1199,3],
+    itemsDesktopSmall : [980,2],
+    itemsTablet: [768,2],
+    autoPlay : 5000,
+    stopOnHover : true,
+    pagination : false
+})
+
+// Testimonials carousel
+$("#owl-testimonial").owlCarousel({
+    singleItem : true,
+    pagination : true,
+    autoHeight : true
+})
+
+
